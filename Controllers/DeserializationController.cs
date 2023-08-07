374a1a85f34d4fa1d3bc5a46ea35ec04ce9a2887
jayant-1998 <jayant.grover@remotestate.com>
2023-08-07 16:42:22 +0530

    first commit

---
+++ Controllers/DeserializationController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeserializationController.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeserializationController : ControllerBase
+    {
+        
+    }
+}
